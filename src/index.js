bdafc89d6c3be1749e3e2944e6360099469293fc
traian-007 <72014521+traian-007@users.noreply.github.com>
2021-03-11 09:36:29 +0200

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from "./App"
+//import HeaderBlock from './components/HeaderBlock'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css';
+
+// const el = React.createElement(
+//   'h1',
+//   null,
+//   'Hello World,ReactJS!!!'
+// );
+
+// const AppList = () => {
+//   const items = ['item1', 'item2', 'item3', 'item4'];
+//   const firstItems = <li>item0</li>;
+//   const isAuth = false;
+//   return (
+//     <ul>
+//       {
+//         isAuth ? firstItems : null
+//       }
+//       {
+//         items.map(item => <li>{item}</li>)
+
+//       }
+//       <li>{items[0]}</li>
+//       <li>{items[1]}</li>
+//     </ul>
+//   )
+// }
+// const AppHeader = () => {
+
+//   return (
+//     <h1
+//       className='header'
+//     >this is my header</h1>
+//   )
+
+// };
+
+// const AppInput = () => {
+//   const placeholder = 'Type text...'
+//   return (
+//     <label htmlFor='search'>
+//       <input id='search' placeholder={placeholder} />
+//     </label>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <HeaderBlock />
+//     </>
+//   )
+// }
+// const el = (
+//   <>
+//     <AppHeader />
+//     <AppList />
+//     <AppHeader />
+//     <AppList />
+//   </>
+// )
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
